84a1f75014edcef75331862adeeb7d819cfec260
ducitp5 <79729479+ducitp5@users.noreply.github.com>
2025-03-27 20:06:12 +0700

    use lombok / @Getter @Setter

--- src/main/java/com/example/hellospringboot/model/Post.java
+++ src/main/java/com/example/hellospringboot/model/Post.java
@@ -5,14 +5,19 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "posts")
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,14 +32,6 @@ public class Post {
         this.content = content;
     }
 
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-    public User getUser() { return user; }
-    public void setUser(User user) { this.user = user; }
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
 //    @JsonBackReference
@@ -44,8 +41,6 @@ public class Post {
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonManagedReference // Allows serialization of comments
     private List<Comment> comments = new ArrayList<>();
-
-    public List<Comment> getComments() { return comments; }
 
     public void addComment(Comment comment) {
         comments.add(comment);
--- src/main/java/com/example/hellospringboot/model/User.java
+++ src/main/java/com/example/hellospringboot/model/User.java
@@ -3,10 +3,15 @@ package com.example.hellospringboot.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "users")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class User {
@@ -24,20 +29,10 @@ public class User {
         this.email = email;
     }
 
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public String getEmail() { return email; }
-
-    public void setId(Long id) { this.id = id; }
-    public void setName(String name) { this.name = name; }
-    public void setEmail(String email) { this.email = email; }
-
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 //    @JsonManagedReference
     @JsonIgnoreProperties("user") // Avoid recursion
     private List<Post> posts = new ArrayList<>();
-
-    public List<Post> getPosts() { return posts; }
 
     // Helper method to add a post
     public void addPost(Post post) {
@@ -58,8 +53,6 @@ public class User {
 //    @JsonManagedReference
     @JsonIgnoreProperties("user") // Avoid recursion
     private Profile profile;
-
-    public Profile getProfile() { return profile; }
 
     public void setProfile(Profile profile) {
         this.profile = profile;
