a050a7546880272005824e32447f87ad694281ad
ducitp5 <79729479+ducitp5@users.noreply.github.com>
2025-05-03 03:56:24 +0700

    config only in MessageSource

--- src/main/java/com/example/hellospringboot/Config/MultiLangues/LocaleConfig.java
+++ src/main/java/com/example/hellospringboot/Config/MultiLangues/LocaleConfig.java
@@ -1,7 +1,9 @@
 package com.example.hellospringboot.Config.MultiLangues;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,16 +24,19 @@ public class LocaleConfig implements WebMvcConfigurer {
         return slr;
     }
 
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang"); // supports ?lang=vi or ?lang=en
-        return lci;
-    }
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(localeChangeInterceptor());
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang"); // supports ?lang=vi or ?lang=en
+        registry.addInterceptor(lci);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages1"); // no .properties extension
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
 
