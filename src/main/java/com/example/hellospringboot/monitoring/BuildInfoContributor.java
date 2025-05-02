837fb7882d38f2deea2a71f137f5c3a13245591d
ducitp5 <79729479+ducitp5@users.noreply.github.com>
2025-05-03 04:37:29 +0700

    POST localhost:8090/actuator/shutdown

---
+++ src/main/java/com/example/hellospringboot/monitoring/BuildInfoContributor.java
@@ -0,0 +1,18 @@
+package com.example.hellospringboot.monitoring;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BuildInfoContributor implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        Map<String,String> data= new HashMap<String,String>();
+        data.put("build.version", "2.0.0.M7");
+        builder.withDetail("buildInfo", data);
+    }
+}
