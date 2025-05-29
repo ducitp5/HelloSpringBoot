a0fab47134533afc9b172682e3b14c12043db1c2
ducitp5 <79729479+ducitp5@users.noreply.github.com>
2025-05-30 02:31:21 +0700

    Spring Boot

---
+++ SpringBootLoginAppDeepSeek/src/main/java/com/example/springbootloginappdeepseek/SpringBootLoginAppDeepSeekApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootloginappdeepseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoginAppDeepSeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoginAppDeepSeekApplication.class, args);
+    }
+
+}
