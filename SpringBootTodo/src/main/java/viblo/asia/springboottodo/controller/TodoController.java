b2008a17fef8cc4e3d89fdc928f70950f8a9b7fd
ducitp5 <79729479+ducitp5@users.noreply.github.com>
2025-06-07 04:25:49 +0700

    add to DB

---
+++ SpringBootTodo/src/main/java/viblo/asia/springboottodo/controller/TodoController.java
@@ -0,0 +1,48 @@
+package viblo.asia.springboottodo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import viblo.asia.springboottodo.Entity.Todo;
+import viblo.asia.springboottodo.service.TodoService;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/db")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    /*
+    @RequestParam dùng để đánh dấu một biến là request param trong request gửi lên server.
+    Nó sẽ gán dữ liệu của param-name tương ứng vào biến
+     */
+    @GetMapping("/listTodo")
+    public String index(Model model, @RequestParam(value = "limit", required = false) Integer limit) {
+        // Trả về đối tượng todoList.
+        model.addAttribute("todoList", todoService.findAll(limit));
+        // Trả về template "listTodo.html"
+        return "db/listTodo";
+    }
+
+    @GetMapping("/addTodo")
+    public String addTodo(Model model) {
+        model.addAttribute("todo", new Todo());
+        return "db/addTodo";
+    }
+
+    /*
+    @ModelAttribute đánh dấu đối tượng Todo được gửi lên bởi Form Request
+     */
+    @PostMapping("/addTodo")
+    public String addTodo(@ModelAttribute Todo todo) {
+        return Optional.ofNullable(todoService.add(todo))
+                .map(t -> "db/success") // Trả về success nếu save thành công
+                .orElse("failed"); // Trả về failed nếu không thành công
+
+    }
+}
+
