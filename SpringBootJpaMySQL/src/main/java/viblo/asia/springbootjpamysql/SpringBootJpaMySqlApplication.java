c9f2cd7e81ebca0b238d371a5f51565bee56974a
ducitp5 <79729479+ducitp5@users.noreply.github.com>
2025-06-06 19:24:19 +0700

    getBean from ApplicationContext

--- SpringBootJpaMySQL/src/main/java/viblo/asia/springbootjpamysql/SpringBootJpaMySqlApplication.java
+++ SpringBootJpaMySQL/src/main/java/viblo/asia/springbootjpamysql/SpringBootJpaMySqlApplication.java
@@ -2,12 +2,49 @@ package viblo.asia.springbootjpamysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.RequiredArgsConstructor;
+import viblo.asia.springbootjpamysql.entity.User;
+import viblo.asia.springbootjpamysql.repository.UserRepository;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class SpringBootJpaMySqlApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootJpaMySqlApplication.class, args);
+
+        ApplicationContext context = SpringApplication.run(SpringBootJpaMySqlApplication.class, args);
+
+        UserRepository userRepository = context.getBean(UserRepository.class);
+
+        // Lấy ra toàn bộ user trong db
+//        userRepository.findAll().forEach(System.out::println);
+
+        // Lưu user xuống database
+        User user = userRepository.save(new User());
+        // Khi lưu xong, nó trả về User đã lưu kèm theo Id.
+        System.out.println("User vừa lưu có ID: " + user.getId());
+        Long userId = user.getId();
+        // Cập nhật user.
+        user.setAgi(100);
+        // Update user
+        // Lưu ý, lúc này đối tượng user đã có Id.
+        // Nên nó sẽ update vào đối tượng có Id này
+        // chứ không insert một bản ghi mới
+        userRepository.save(user);
+
+        // Query lấy ra user vừa xong để kiểm tra xem.
+        User user2 = userRepository.findById(userId).get();
+        System.out.println("User: " + user);
+        System.out.println("User2: " + user2);
+
+        // Xóa User khỏi DB
+        userRepository.delete(user);
+
+        // In ra kiểm tra xem userId còn tồn tại trong DB không
+        User user3 = userRepository.findById(userId).orElse(null);
+        System.out.println("User3: " + user3);
     }
 
 }
